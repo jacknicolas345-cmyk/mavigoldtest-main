e5e326f5e6375ba4d727a6068be7257e14ec3298
jacknicolas345-cmyk <jacknicolas345@gmail.com>
2026-05-02 17:23:18 +0330

    Add User model with schema definition

---
+++ lib/models/User.ts
@@ -0,0 +1,13 @@
+import mongoose, { Schema, models } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: String,
+    email: { type: String, unique: true },
+    password: String,
+    role: { type: String, default: "customer" }
+  },
+  { timestamps: true }
+);
+
+export default models.User || mongoose.model("User", userSchema);
