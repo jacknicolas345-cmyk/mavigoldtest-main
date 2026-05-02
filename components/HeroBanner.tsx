6ab81029951ca29bf7ecdd550482f1fcb0df9fb9
jacknicolas345-cmyk <jacknicolas345@gmail.com>
2026-05-02 17:11:54 +0330

    Add HeroBanner component for gold and jewelry store

---
+++ components/HeroBanner.tsx
@@ -0,0 +1,7 @@
+export default function HeroBanner() {
+  return (
+    <div className="relative h-72 bg-gray-200 flex items-center justify-center">
+      <h1 className="text-4xl font-bold">فروشگاه طلا و جواهر</h1>
+    </div>
+  );
+}
